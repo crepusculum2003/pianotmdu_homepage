66184d38b763cf908562c961b3b8f5bece184dd1
crepusculum2003 <yuyukosamakyou@gmail.com>
2023-09-28 10:42:56 +0900

    ver 0.2

--- src/article/Contact.tsx
+++ src/article/Contact.tsx
@@ -28,7 +28,7 @@ const Contact = () => {
           </div>
           <div className="Access_map">
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2526.639673542707!2d139.762325321401!3d35.701783469928046!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188c1906bd3e7f%3A0x37495cf14bf681b6!2z5p2x5Lqs5Yy756eR5q2v56eR5aSn5a2m55Sf5rS75Y2U5ZCM57WE5ZCIIOa5r-Wztumjn-Wggg!5e0!3m2!1sja!2sjp!4v1695773007009!5m2!1sja!2sjp" 
-            width="600" height="450" loading="lazy" >google map</iframe>
+            loading="lazy" className="googlemap" >google map</iframe>
           </div>
         </div>
 
